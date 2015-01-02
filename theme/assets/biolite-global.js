248ef2ae55926d2c75efa144c31e869b4ef73714
Tracy <tracy@fiftyandfifty.org>
2015-01-02 15:08:04 -0800

    working on product page

--- theme/assets/biolite-global.js
+++ theme/assets/biolite-global.js
@@ -5,6 +5,14 @@ BioliteGlobal.init = function()
 	BioliteGlobal.loaded = true;
 }
 
+BioliteGlobal.get_product = function(id)
+{
+	jQuery.getJSON('/products/' + id + '.js', function(product) {
+		console.log(product)
+	}
+}
+
+
 $(function() {
 	BioliteGlobal.init();
 });
